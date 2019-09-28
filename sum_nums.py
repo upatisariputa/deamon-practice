2ef88b531a3d1a6f7e709e44200e7d871a666c54
sitaruta <upatisariputa@gmail.com>
2019-09-28 15:51:49 +0900

    deamon-practice

---
+++ sum_nums.py
@@ -0,0 +1,21 @@
+import time
+
+start_time = time.time()
+
+
+def fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibo(n-1) + fibo(n-2)
+
+
+num_list = [32, 33, 34, 35]
+result_list = []
+for num in num_list:
+    result_list.append(fibo(num))
+
+print(result_list)
+print("--- %s seconds ---" % (time.time() - start_time))
